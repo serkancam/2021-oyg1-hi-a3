9daa64bee8c9873e93bb7780f53b0d8f975d6c4a
serkancam <sekocam@gmail.com>
2021-10-05 17:41:58 +0300

    t05102021

---
+++ hatirlatma.py
@@ -0,0 +1,17 @@
+import random
+import numpy as np
+
+print("merhaba")
+print   ("merhaba")
+print   (        "merhaba")
+print   (        "merhaba"        )
+print("merhaba")
+print("merhaba")
+print("merhaba","mustafa","nasılsın?",end="\n")
+print("merhaba","mustafa","nasılsın?",sep="@")
+
+ad="serkan"
+yas=39
+
+print("merhaba",ad,"nasılsın?",yas," yaşı nasıl?")
+print(f"merhaba {ad} nasılsın? {yas} yaşı nasıl?")
