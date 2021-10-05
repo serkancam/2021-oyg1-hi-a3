834117771265840ea8e6094806a7970c700e1736
serkancam <sekocam@gmail.com>
2021-10-05 18:24:38 +0300

    t05102021

---
+++ degiskenler.py
@@ -0,0 +1,15 @@
+mustafa=150
+keyfi=350
+ve_kahyasi=400
+
+# mustafa keyfi ve kahyası paralarını eşit biçimde paylaşırşarsa
+# her birinin kaç parası olur.
+hbp=(mustafa+keyfi+ve_kahyasi)/3
+print(f"ortalama hbp={hbp}")
+
+# mustsafa tanesi 13 tl olan defterlerden en fazla kaç adet alabilir. 
+# geriye kaç parası kalır.
+
+fiyat=13
+adet=?
+kalan=?
